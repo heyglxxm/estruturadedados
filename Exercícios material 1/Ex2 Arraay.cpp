cbdec5d3f3472697f806bc0d9abd5d66250a1c61
heyglxxm <dufersilva0133@gmail.com>
2026-02-20 22:13:47 -0300

    Exercicios lista 1

---
+++ Exercícios material 1/Ex2 Arraay.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+#define qnt_notas 2
+
+int main (){
+    float notas [qnt_notas];
+    float media=0;
+    int acimaMedia=0;
+
+
+    for (int i=0;i <qnt_notas;i++){
+        cout<<"Insira a "<<i+1<<" nota:"<<endl;
+        cin>>notas[i];
+        media += notas[i];
+    }
+
+    media/=qnt_notas;
+
+    for (int i=0;i<qnt_notas;i++){
+        if (notas[i]>=media)
+        acimaMedia++;
+    }
+
+    cout<<"A media da turma e: "<<media<<endl;
+    cout<<"A quantia de alunos acima da media e: "<<acimaMedia<<endl;
+
+}
